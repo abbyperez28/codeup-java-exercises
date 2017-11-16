f88e9edd938971ccef0e353186d6d422c6e3ec95
abbyperez <abbyperez28@gmail.com>
2017-11-16 16:57:43 -0600

    EXERCISE: Java Syntax, Data Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,81 @@
-public class HelloWorld {
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
+//public class HelloWorld {
+//        public static void main(String[] args) {
+//            System.out.println("Hello, World!");
+
+public class Main {
+    public static void main(String[] args) {
+
+        int myFavoriteNumber = 11;
+            System.out.println(myFavoriteNumber);
+            //prints out 11
+//
+            String myString = "Awesome";
+            System.out.println(myString);
+//        //prints out "Awesome"
+            System.out.println("My favorite string is " + myString);
+//        prints out My favorite string is Awesome
+
+            //**********YOU CANT ASSIGN CHARS TO A STRING VARIABLE*************
+//
+            long myNumber;
+            System.out.println(myNumber);
+////        prints out error: variable not initialized"
+
+            //*********NEED TO ASSIGN A VALUE FOR THE FIRST TIME TO A VARIABLE BEFORE WE DO ANYTHING*********
+
+            //**********ONCE A VARIABLE IS CREATED, YOU DO NOT HAVE TO LABEL WHAT KIND OF VARIABLE IT IS AGAIN*********
+            myNumber = 3.14;
+            System.out.println(myNumber);
+////        prints out "error: incompatible types"
+//
+            myNumber = 123L;
+            System.out.println(myNumber);
+//        //prints out numbers without letter "L"; Note: "L" refers to "long"
+//
+            long myNumber = 123;
+            System.out.println(myNumber);
+            //prints out 123
+
+            float myNumber = 3.14;
+            System.out.println(myNumber);
+            //prints out incompatible type
+
+            int x = 5;
+            System.out.println(x++); //increment made AFTER
+            System.out.println(x);
+//        //prints 5, 6
+
+            int x = 5;
+            System.out.println(++x); //increment made before
+            System.out.println(x); //x refers to new number
+//        //prints out 6,6
+
+            int x = 4;
+            x = x + 5;
+            System.out.println(x);
+
+            x += 5; //SHORTHAND
+//        //prints out 9
+
+            int x = 3;
+            int y = 4;
+            y = y * x;
+            System.out.println(y);
+
+            y *= x; //SHORTHAND
+            //prints out 12
+
+            int x = 10;
+            int y = 2;
+            x = x / y;
+            y = y - x;
+            System.out.println(x);
+            System.out.println(y);
+            //prints out 5, -3
+
+        }
+}
+
         }
 }
 
