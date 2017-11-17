660207505ef423f16087b1a42fc7f794f7535498
abbyperez <abbyperez28@gmail.com>
2017-11-17 16:37:12 -0600

    Control Flow Exercise: FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,15 +2,15 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-
-        //WHILE LOOP
-        int i = 5;
-
-        while (i < 15) {
-            System.out.println(i);
-            i++;
-            System.out.println(i);
-        }
+//
+//        //WHILE LOOP
+//        int i = 5;
+//
+//        while (i < 15) {
+//            System.out.println(i);
+//            i++;
+//            System.out.println(i);
+//        }
 
         //DO-WHILE LOOP
 //        int x = 2;
@@ -28,17 +28,30 @@ public class ControlFlowExercises {
 //            x -= 5;
 //        } while (x >= -10);
 
-        long y = 2;
+//        long y = 2;
+//
+//        do{
+//            System.out.println(y);
+//            y = y * y;
+////            square = Math.pow(y, 2);
+//        } while (y < 1000000);
+//
 
-        do{
-            System.out.println(y);
-            y = y * y;
-//            square = Math.pow(y, 2);
-        } while (y < 1000000);
-
+//public class fizzbuzz {
+//    public static void main(String[] args){
+        for(int i = 1; i <= 100; i++){
+            String test = "";
+            test += (i % 3) == 0 ? "fizz" : "";
+            test += (i % 5) == 0 ? "buzz" : "";
+            System.out.println(!test.isEmpty() ? test : i);
+        }
 
 
     }
 }
+
+
+
+
 
 
