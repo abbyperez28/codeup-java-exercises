9b40ed40199ad3bb743a4baa5599b8c7dbc4c2d2
abbyperez <abbyperez28@gmail.com>
2017-11-22 09:43:05 -0600

    continuation of methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+//    int a = 11;
+//    int b = 10;
+//        System.out.println(i + j);
+//        System.out.println(i - j);
+//        System.out.println(i * j);
+//        System.out.println(i / j);
+//        System.out.println(i % j);
+
+//        System.out.println(i / j);
+        //^^^^ j =0 ^^^^ console: "exception in thread"
+
+//    Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter first number: ");
+//
+//        int num1 = scan.nextInt();
+//
+//        System.out.print("Enter second number: ");
+//        int num2 = scan.nextInt();
+//
+//        scan.close();
+//
+//        int product = num1 * num2;
+//
+//        System.out.println("Output: " + product);
+
+
+//        Scanner scan = new Scanner(System.in);
+//        int number;
+//
+//        do {
+//            System.out.print("Enter a number between 1 and 10: ");
+//        } while (!sc.hasNextInt()) {
+//            System.out.println("Enter a number bewtween 1 and 10: ");
+//            sc.next();
+//        }
+//    }
+//
+//    private static boolean isValid(int number) {
+//        int minValue = 1;
+//        int maxValue = 10;
+//
+//        if (number < minValue || number > maxValue) {
+//            System.out.println("Enter a number between 1 and 10: ");
+//            return false;
+//        } else {
+//            return true;
+//        }
+
+
+    }
+}
+
+
+
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,27 +2,27 @@ public class StringExercise {
     public static void main(String[] args) {
 
 
-String message;
-        message = "We don't need no education \n";
-        message += "We don't need not thought control";
-        message += "\n\n";
+//String message;
+//        message = "We don't need no education \n";
+//        message += "We don't need not thought control";
+//        message += "\n\n";
+////        System.out.println(message);
+//
+//        message += "Check \"this \"out!, \" inside of \"s!";
+//        message += "\n\n";
+//
+////        System.out.println(message);
+//
+//        message += "In windows, the main drive is usually C:\\";
+//        message += "\n\n";
+//
+////        System.out.println(message);
+//
+//        message += "I can do backslashes \, double backslashes \\,\n;" +
+//        message += "and the amazing triple backslash \\\!";
+//        message += "\n\n";
+//
 //        System.out.println(message);
-
-        message += "Check \"this \"out!, \" inside of \"s!";
-        message += "\n\n";
-
-//        System.out.println(message);
-
-        message += "In windows, the main drive is usually C:\\";
-        message += "\n\n";
-
-//        System.out.println(message);
-
-        message += "I can do backslashes \, double backslashes \\,\n;" +
-        message += "and the amazing triple backslash \\\!";
-        message += "\n\n";
-
-        System.out.println(message);
 
 
 
