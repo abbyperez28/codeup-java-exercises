d6c6b8403987545ceabe01927df2440981fbf666
abbyperez <abbyperez28@gmail.com>
2017-11-28 12:19:03 -0600

    Java Arrays Exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,36 @@
+public class ServerNameGenerator {
+
+
+
+        String[] adjectives {
+            "Good",
+            "New",
+            "First",
+            "Last",
+            "Long",
+            "Great",
+            "Little",
+            "Own",
+            "Other",
+            "Old"
+        };
+
+
+        String[] nouns {
+            "Woman",
+            "Mountain",
+            "State",
+            "Love",
+            "Happiness",
+            "Cat",
+            "Building",
+            "Bed",
+            "Phone",
+            "Pen"
+        };
+
+
+    public static void main(String[] args) {
+
+    }
+}
